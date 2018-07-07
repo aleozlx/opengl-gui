170a26999a24a0ab9527a02994486572abfc3531
Alex Yang <aleozlx@gmail.com>
2018-07-06 23:44:28 -0500

    init

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    // ui.window(im_str!("Hello world"))
+    // .size((300.0, 100.0), ImGuiCond::FirstUseEver)
+    // .build(|| {
+    //     ui.text(im_str!("Hello world!"));
+    //     ui.text(im_str!("This...is...imgui-rs!"));
+    //     ui.separator();
+    //     let mouse_pos = ui.imgui().mouse_pos();
+    //     ui.text(im_str!("Mouse Position: ({:.1},{:.1})", mouse_pos.0, mouse_pos.1));
+    // });
+}
