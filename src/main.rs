17f60d349ca6d95083e5872263a2492a88f7cfcf
Alex Yang <aleozlx@gmail.com>
2018-07-17 18:21:00 -0500

    switch to gtk

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,29 @@
-extern crate gfx;
-extern crate gfx_window_glutin;
-extern crate glutin;
-#[macro_use]
-extern crate imgui;
-extern crate imgui_gfx_renderer;
-extern crate imgui_sys;
-
-use gfx::traits::FactoryExt;
-use gfx::Device;
-use gfx_window_glutin as gfx_glutin;
-use imgui::*;
-
-// mod support_gfx;
+extern crate gtk;
+use gtk::prelude::*;
+use gtk::{Button, Window, WindowType};
 
 fn main() {
-    support_gfx::run("hello_gfx.rs".to_owned(), CLEAR_COLOR, hello_world);
+    if gtk::init().is_err() {
+        println!("Failed to initialize GTK.");
+        return;
+    }
+
+    let window = Window::new(WindowType::Toplevel);
+    window.set_title("First GTK+ Program");
+    window.set_default_size(350, 70);
+    let button = Button::new_with_label("Click me!");
+    window.add(&button);
+    window.show_all();
+
+    window.connect_delete_event(|_, _| {
+        gtk::main_quit();
+        Inhibit(false)
+    });
+
+    button.connect_clicked(|_| {
+        println!("Clicked!");
+    });
+
+    gtk::main();
 }
+
