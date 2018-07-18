a555ee128a8c530988b11acec415eb3abac60a2b
Alex Yang <aleozlx@gmail.com>
2018-07-18 13:55:40 -0500

    test glade

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 extern crate gtk;
 use gtk::prelude::*;
-use gtk::{Button, Window, WindowType, Image};
+use gtk::{Window, WindowType, TreeView};
 
 fn main() {
     if gtk::init().is_err() {
@@ -8,24 +8,18 @@ fn main() {
         return;
     }
 
-    let window = Window::new(WindowType::Toplevel);
-    window.set_title("First GTK+ Program");
+    let glade_src = include_str!("ui-design.glade");
+    let builder = gtk::Builder::new_from_string(glade_src);
+    let window: gtk::Window = builder.get_object("main_window").unwrap();
+    window.set_title("UI Design");
     window.set_default_size(350, 70);
-    let button = Button::new_with_label("Click me!");
-    // window.add(&button);
-    // ref: http://gtk-rs.org/docs/gtk/
-    let image = Image::new_from_file("/home/alex/3cv-blog/static/icons/favicon-32x32.png");
-    window.add(&image);
+
+    let nn_layers: gtk::TreeView = builder.get_object("nn_layers").unwrap();
+
+    // // ref: http://gtk-rs.org/docs/gtk/
+    // let image = Image::new_from_file("/home/alex/3cv-blog/static/icons/favicon-32x32.png");
+    // window.add(&image);
     window.show_all();
-
-    window.connect_delete_event(|_, _| {
-        gtk::main_quit();
-        Inhibit(false)
-    });
-
-    button.connect_clicked(|_| {
-        println!("Clicked!");
-    });
 
     gtk::main();
 }
