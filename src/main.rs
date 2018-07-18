48efb462c9f68998febfbeff38ed20134d4cbd48
Alex Yang <aleozlx@gmail.com>
2018-07-18 13:11:13 -0500

    test image widget

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 extern crate gtk;
 use gtk::prelude::*;
-use gtk::{Button, Window, WindowType};
+use gtk::{Button, Window, WindowType, Image};
 
 fn main() {
     if gtk::init().is_err() {
@@ -12,7 +12,10 @@ fn main() {
     window.set_title("First GTK+ Program");
     window.set_default_size(350, 70);
     let button = Button::new_with_label("Click me!");
-    window.add(&button);
+    // window.add(&button);
+    // ref: http://gtk-rs.org/docs/gtk/
+    let image = Image::new_from_file("/home/alex/3cv-blog/static/icons/favicon-32x32.png");
+    window.add(&image);
     window.show_all();
 
     window.connect_delete_event(|_, _| {
