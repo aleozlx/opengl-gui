289f4507b1f69214647f5152e1c3840a1f1ba351
Alex Yang <aleozlx@gmail.com>
2018-07-21 14:00:00 -0500

    refactor

--- src/main.rs
+++ src/main.rs
@@ -5,144 +5,13 @@ extern crate epoxy;
 extern crate shared_library;
 extern crate image;
 
+mod shader_routines;
 use yaml_rust::YamlLoader;
 use gtk::prelude::*;
 use std::ffi::{CString, CStr};
 use shared_library::dynamic_library::DynamicLibrary;
 use image::GenericImage;
-
-struct Shader {
-    id: gl::types::GLuint,
-}
-
-impl Shader {
-    pub fn id(&self) -> gl::types::GLuint {
-        self.id
-    }
-
-    fn from_source(source: &CStr, kind: gl::types::GLenum) -> Result<Shader, String> {
-        let id = shader_from_source(source, kind)?;
-        Ok(Shader { id })
-    }
-
-    fn from_vert_source(source: &CStr) -> Result<Shader, String> {
-        Shader::from_source(source, gl::VERTEX_SHADER)
-    }
-
-    fn from_frag_source(source: &CStr) -> Result<Shader, String> {
-        Shader::from_source(source, gl::FRAGMENT_SHADER)
-    }
-}
-
-impl Drop for Shader {
-    fn drop(&mut self) {
-        unsafe {
-            gl::DeleteShader(self.id);
-        }
-    }
-}
-
-fn create_empty_cstring(len: usize) -> CString {
-    let mut buffer: Vec<u8> = Vec::with_capacity(len + 1);
-    buffer.extend([b' '].iter().cycle().take(len)); // fill it with len spaces
-    unsafe { CString::from_vec_unchecked(buffer) } // convert buffer to CString
-}
-
-fn shader_from_source(source: &CStr, kind: gl::types::GLenum) -> Result<gl::types::GLuint, String> {
-    let id = unsafe { gl::CreateShader(kind) };
-    unsafe {
-        gl::ShaderSource(id, 1, &source.as_ptr(), std::ptr::null());
-        gl::CompileShader(id);
-    }
-    let mut success: gl::types::GLint = 1;
-    unsafe {
-        gl::GetShaderiv(id, gl::COMPILE_STATUS, &mut success);
-    }
-    if success == 0 {
-        let mut len: gl::types::GLint = 0;
-        unsafe {
-            gl::GetShaderiv(id, gl::INFO_LOG_LENGTH, &mut len);
-        }
-        let error = create_empty_cstring(len as usize);
-        unsafe {
-            gl::GetShaderInfoLog(
-                id,
-                len,
-                std::ptr::null_mut(),
-                error.as_ptr() as *mut gl::types::GLchar
-            );
-        }
-        Err(error.to_string_lossy().into_owned())
-    }
-    else{
-        Ok(id)
-    }
-}
-
-struct Program {
-    id: gl::types::GLuint,
-}
-
-impl Program {
-    pub fn from_shaders(shaders: &[Shader]) -> Result<Program, String> {
-        let program_id = unsafe { gl::CreateProgram() };
-
-        for shader in shaders {
-            unsafe { gl::AttachShader(program_id, shader.id()); }
-        }
-
-        unsafe { gl::LinkProgram(program_id); }
-        
-        let mut success: gl::types::GLint = 1;
-        unsafe {
-            gl::GetProgramiv(program_id, gl::LINK_STATUS, &mut success);
-        }
-
-        if success == 0 {
-            let mut len: gl::types::GLint = 0;
-            unsafe {
-                gl::GetProgramiv(program_id, gl::INFO_LOG_LENGTH, &mut len);
-            }
-
-            let error = create_empty_cstring(len as usize);
-
-            unsafe {
-                gl::GetProgramInfoLog(
-                    program_id,
-                    len,
-                    std::ptr::null_mut(),
-                    error.as_ptr() as *mut gl::types::GLchar
-                );
-            }
-
-            return Err(error.to_string_lossy().into_owned());
-        }
-
-        for shader in shaders {
-            unsafe { gl::DetachShader(program_id, shader.id()); }
-        }
-
-        Ok(Program { id: program_id })
-    }
-
-    pub fn id(&self) -> gl::types::GLuint {
-        self.id
-    }
-
-    fn use_program(&self) {
-        unsafe {
-            gl::UseProgram(self.id);
-        }
-    }
-}
-
-impl Drop for Program {
-    fn drop(&mut self) {
-        unsafe {
-            gl::DeleteProgram(self.id);
-        }
-    }
-}
+use shader_routines::{Shader, ShaderProgram};
 
 fn append_column(tree: &gtk::TreeView, id: i32) {
     let column = gtk::TreeViewColumn::new();
@@ -187,12 +56,12 @@ fn prepare_visualization() -> gl::types::GLuint {
         &CString::new(include_str!("canvas.frag")).unwrap()
     ).unwrap();
 
-    let shader_program = Program::from_shaders(
+    let shader_program = ShaderProgram::from_shaders(
         &[vert_shader, frag_shader]
     ).unwrap();
 
     shader_program.use_program();
-    
+   
     let vertices: Vec<f32> = vec![
     	-1.0, 1.0, 0.0, 0.0, // Top-left
         1.0, 1.0, 1.0, 0.0, // Top-right
@@ -245,25 +114,26 @@ fn prepare_visualization() -> gl::types::GLuint {
         gl::EnableVertexAttribArray(tex_attr);
         gl::VertexAttribPointer(tex_attr, 2, gl::FLOAT, gl::FALSE, (4 * std::mem::size_of::<f32>()) as gl::types::GLint, (2 * std::mem::size_of::<f32>()) as *const std::os::raw::c_void);
 
-        let img = image::open("/home/alex/Downloads/avatar64.jpg").unwrap();
-        let img_rgb = img.to_rgb().into_raw();
-        let (width, height) = img.dimensions();
-        let mut texture: gl::types::GLuint = 0;
-        gl::GenTextures(1, &mut texture);
-        gl::BindTexture(gl::TEXTURE_2D, texture);
-        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_S, gl::REPEAT as i32);
-        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T, gl::REPEAT as i32);
-        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MIN_FILTER, gl::LINEAR as i32);
-        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::LINEAR as i32);
-        // let inputFormat: gl::types::GLenum = greyscale ? GL_LUMINANCE : GL_BGR;
-        gl::PixelStorei(gl::UNPACK_ALIGNMENT, 1);
-        gl::PixelStorei(gl::UNPACK_ROW_LENGTH, width as i32);
-        gl::TexImage2D(gl::TEXTURE_2D, 0, gl::RGB as i32, width as i32, height as i32, 0, gl::RGB, gl::UNSIGNED_BYTE, img_rgb.as_ptr() as *const std::os::raw::c_void);
+        // let img = image::open("/home/alex/Downloads/avatar64.jpg").unwrap();
+        // let img_rgb = img.to_rgb().into_raw();
+        // let (width, height) = img.dimensions();
 
-        gl::BindBuffer(gl::ARRAY_BUFFER, 0);
-        gl::BindVertexArray(0);
+        // let mut texture: gl::types::GLuint = 0;
+        // gl::GenTextures(1, &mut texture);
+        // gl::BindTexture(gl::TEXTURE_2D, texture);
+        // gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_S, gl::REPEAT as i32);
+        // gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T, gl::REPEAT as i32);
+        // gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MIN_FILTER, gl::LINEAR as i32);
+        // gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::LINEAR as i32);
+        // // let inputFormat: gl::types::GLenum = greyscale ? GL_LUMINANCE : GL_BGR;
+        // gl::PixelStorei(gl::UNPACK_ALIGNMENT, 1);
+        // gl::PixelStorei(gl::UNPACK_ROW_LENGTH, width as i32);
+        // gl::TexImage2D(gl::TEXTURE_2D, 0, gl::RGB as i32, width as i32, height as i32, 0, gl::RGB, gl::UNSIGNED_BYTE, img_rgb.as_ptr() as *const std::os::raw::c_void);
+    
+        // gl::BindBuffer(gl::ARRAY_BUFFER, 0);
+        // gl::BindVertexArray(0);
 
-        gl::ClearColor(0.0, 0.0, 0.0, 1.0);
+        // gl::ClearColor(0.0, 0.0, 0.0, 1.0);
     }
 
     return shader_program.id();
@@ -319,7 +189,7 @@ fn main() {
 
     canvas.connect_render(move |_, _| {
         unsafe {
-            gl::Clear(gl::COLOR_BUFFER_BIT);
+            // gl::Clear(gl::COLOR_BUFFER_BIT);
             // let time_elapsed = std::time::SystemTime::now().duration_since(time_start).unwrap();
 
             gl::BindVertexArray(g_vertex_array);
@@ -331,7 +201,6 @@ fn main() {
 
         Inhibit(false)
     });
-    // glib::signal::connect(&canvas, "render", G_CALLBACK (render), None);
 
     window.show_all();
     window.connect_delete_event(|_, _| {
